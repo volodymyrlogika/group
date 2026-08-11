2ffa88e5d67505f9530c8d892abfbf83cd9e97b1
name090 <mukolanimets0@gmail.com>
2026-08-11 19:16:51 +0300

    початок створеня "календара подій"

---
+++ calendar/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+from .models import Event
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ['title', 'description', 'location', 'start_time', 'end_time']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'location': forms.TextInput(attrs={'class': 'form-control'}),
+            'start_time': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+            'end_time': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
+        }
