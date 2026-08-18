8df1abf1e0ddbe9f1fb2bc291eeb09b89442ba5d
2019maksumm2019-png <2019maksumm2019>
2026-08-18 19:46:30 +0300

    portfolio.html

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,5 +9,8 @@ from .views import (
 
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project_list'),
-    
+    path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('create/', ProjectCreateView.as_view(), name='project_create'),
+    path('<int:pk>/update/', ProjectUpdateView.as_view(), name='project_update'),
+    path('<int:pk>/delete/', ProjectDeleteView.as_view(), name='project_delete'),
 ]
