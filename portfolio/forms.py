f35a70b1dc35ccffc67513026d170d80765c0dfa
2019maksumm2019-png <2019maksumm2019>
2026-08-18 20:23:18 +0300

    forms.py

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Project, ProjectImage
-
+from django.forms import CheckboxInput
 class ProjectForm(forms.ModelForm):
-    best_project = forms.BooleanField(required=False, label='Best Project')
+    best_project = forms.BooleanField(required=False, label='Best Project', widget=forms.CheckboxInput(attrs={'class': 'form-check-input'}))
     class Meta:
         model = Project
         fields = ['name', 'description', 'project_link', 'files', 'best_project', 'technologies']
@@ -10,7 +10,7 @@ class ProjectForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
-            if field.widget.attrs.get('type') == 'checkbox':
+            if isinstance(field.widget, CheckboxInput):
                 field.widget.attrs['class'] = 'form-check-input'
             else:
                 field.widget.attrs['class'] = 'form-control'
