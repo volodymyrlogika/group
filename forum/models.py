d34062ff094ea554e1bb2d5699f774fd340a078f
Danik5017 <dgg16626@gmail.com>
2026-08-18 19:03:41 +0300

    add models, forms

---
+++ forum/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Thread(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Назва гілки")
+    description = models.TextField(blank=True, verbose_name="Опис гілки")
+    created_at = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='threads')
+
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField(verbose_name="Текст повідомлення")
+    image = models.ImageField(upload_to="forum_photos/", blank=True, null=True, verbose_name="Фото")
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_posts', blank=True)
+
+    def __str__(self):
+        return f"Повідомлення від {self.author.username}"
