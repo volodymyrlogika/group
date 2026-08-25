49b58413d959eda17ad51bbd6322946ce5209786
vova916 <volodumur086@gmail.com>
2026-08-25 20:10:56 +0300

    доробив точно

--- systemreq/forms.py
+++ systemreq/forms.py
@@ -1,21 +1,22 @@
 from django import forms
-from .models import Servey
-from .models import AnswerOption , Quisetion
+from .models import AnswerOption, Quisetion
 
 class TakeSurveyForm(forms.Form):
 
-  def __init__(self, *args, **kwargs):
-    questions = kwargs.pop("questions", [])
-    super().__init__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        questions = kwargs.pop("questions", [])
+        super().__init__(*args, **kwargs)
 
-    for question in questions:
-      options = AnswerOption.objects.filter(quition=question)
-      choices = [(opt.id, opt.text) for opt in options]
+        for question in questions:
+            options = AnswerOption.objects.filter(quition=question)
+            choices = [(opt.id, opt.text) for opt in options]
 
-      self.fields[f"question_{question.id}"] = forms.ChoiceField(
-          label=question.text,
-          choices=choices,
-          widget=forms.RadioSelect(attrs={"class": "form-check-input"}),
-          required=True,
-      )
-
+            field_name = f"question_{question.id}"
+            self.fields[field_name] = forms.ChoiceField(
+                label=question.text,
+                choices=choices,
+                widget=forms.RadioSelect(attrs={"class": "form-check-input"}),
+                required=True,
+            )
+            # Прив'язуємо об'єкт питання прямо до поля форми!
+            self.fields[field_name].question = question
