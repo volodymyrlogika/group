ce50e200c761c49a6d5553466b045e7183fee56c
Danik5017 <dgg16626@gmail.com>
2026-08-25 19:58:04 +0300

    додано додавання тредів адмінами, гортання сторінок

--- forum/views.py
+++ forum/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.http import JsonResponse
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator
 from .models import Thread, Post
 from .forms import ThreadForm, PostForm
 
@@ -12,13 +13,45 @@ def is_staff_user(user):
     raise PermissionDenied
 
 def forum_home(request):
-    threads = Thread.objects.all().order_by('-created_at')
-    return render(request, 'forum/home.html', {'threads': threads})
+    threads_list = Thread.objects.all().order_by('-created_at')
+    
+    # Кількість тредів на сторінку (залиште 1 для тесту або змініть на 20)
+    paginator = Paginator(threads_list, 1) 
+    
+    page_number = request.GET.get('page', 1)
+    try:
+        page_number = int(page_number)
+    except ValueError:
+        page_number = 1
+        
+    threads = paginator.get_page(page_number)
+    total_pages = paginator.num_pages
+
+    # 🟢 РОЗРАХУНОК ФІКСОВАНИХ 5 СТОРІНОК ДЛЯ ШАБЛОНУ
+    if total_pages <= 5:
+        page_range = range(1, total_pages + 1)
+    else:
+        start = page_number - 2
+        end = page_number + 2
+        
+        # Перевірка меж, щоб завжди виводилося рівно 5 кнопок
+        if start < 1:
+            start = 1
+            end = 5
+        elif end > total_pages:
+            end = total_pages
+            start = total_pages - 4
+            
+        page_range = range(start, end + 1)
+
+    return render(request, 'forum/home.html', {
+        'threads': threads,
+        'custom_page_range': page_range
+    })
 
 def thread_detail(request, thread_id):
     thread = get_object_or_404(Thread, id=thread_id)
     posts = thread.posts.all().order_by('created_at')
-
 
     if request.method == 'POST' and request.user.is_authenticated:
         post_form = PostForm(request.POST, request.FILES)
